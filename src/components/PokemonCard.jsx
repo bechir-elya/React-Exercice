5dceff25f479f45586a21a0da4d6b4286512057f
bechir-elya <bechir.elya@gmail.com>
2024-07-23 12:32:18 +0200

    Rendre le composant dynamique

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from 'react';
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function PokemonCard() {
+  const pokemon = pokemonList[0];
+
   return (
     <>
-      <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+      <figure style={{ backgroundColor: 'lightgreen', borderRadius: '10px' }}>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
     </>
-  )
+  );
 }
 
-export default PokemonCard
+export default PokemonCard;
