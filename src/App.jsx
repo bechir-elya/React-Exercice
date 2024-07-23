ee78f00674decfc9b31c1960f371bbc8f8b85aa0
bechir-elya <bechir.elya@gmail.com>
2024-07-23 15:28:18 +0200

    using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
@@ -8,17 +9,43 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
 
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const nextPokemon = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  }
+
+  const previousPokemon = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  }
 
   return (
     <>
       <div>
-        <PokemonCard pokemon={pokemonList[0]}/>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+        <button onClick={previousPokemon} style={{ display: pokemonIndex > 0 ? 'inline-block' : 'none' }}>Précédent</button>
+        <button onClick={nextPokemon} style={{ display: pokemonIndex < pokemonList.length -1 ? 'inline-block' : 'none' }}>Suivant</button>
       </div>
     </>
   )
